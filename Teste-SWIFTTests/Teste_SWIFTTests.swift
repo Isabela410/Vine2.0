a60bd0546805ce16652f4449f76a93adacfb38e1
Izabour Azevedo <110848858+IzabourAzevedo@users.noreply.github.com>
2025-02-05 14:31:22 -0300

    Initial Commit

---
+++ Teste-SWIFTTests/Teste_SWIFTTests.swift
@@ -0,0 +1,17 @@
+//
+//  Teste_SWIFTTests.swift
+//  Teste-SWIFTTests
+//
+//  Created by izabour Azevedo on 04/02/25.
+//
+
+import Testing
+@testable import Teste_SWIFT
+
+struct Teste_SWIFTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
