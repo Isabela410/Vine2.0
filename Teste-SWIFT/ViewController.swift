53f20b939b903a3779a27e5f3a23af17e7140070
Izabour Azevedo <110848858+IzabourAzevedo@users.noreply.github.com>
2025-02-05 14:31:22 -0300

    Initial Commit

---
+++ Teste-SWIFT/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Teste-SWIFT
+//
+//  Created by izabour Azevedo on 04/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var passwordEmpty: UITextField!
+    @IBOutlet weak var closeOpen: UIButton!
+    @IBAction func buttonEye(_ sender: Any) {
+        passwordEmpty.isSecureTextEntry.toggle()
+        _ = passwordEmpty.isSecureTextEntry ? "eye.slash" : "eye.fill"
+    }
+@objc func buttonCloseOpen(_ sender: Any) {
+            
+    }
+        
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
+        }
+        
+    }
+
