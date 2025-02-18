991334f33122f8dfc08e9c0db8c379bae59e6bbc
Izabour Azevedo <110848858+IzabourAzevedo@users.noreply.github.com>
2025-02-18 14:34:30 -0300

    navegação das telas

---
+++ Teste-SWIFT/View/FeedUser/FeedUserViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  FeedUserViewCell.swift
+//  Teste-SWIFT
+//
+//  Created by izabour Azevedo on 18/02/25.
+//
+
+import UIKit
+
+class FeedUserViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
